0c2b877e4f30dd9985edc3304e88e1a9b640ef5d
maci <mogyorosdavid@gmail.com>
2023-02-24 13:23:19 +0100

    🎨 Moved DefaultBall into the Ball file

--- ball.go
+++ ball.go
@@ -19,3 +19,15 @@ func (b *Ball) Draw() {
 func Colliding(b *Ball, p *Player) bool {
 	return rl.CheckCollisionRecs(b.rect, p.paddleRect)
 }
+
+func DefaultBall(g *Game, side float32) Ball {
+	var ballSize float32 = 15
+
+	return Ball{
+		rect: rl.NewRectangle(float32(g.config.WindowWidth)/2-ballSize/2, float32(g.config.WindowHeight/2)-ballSize/2, ballSize, ballSize),
+
+		dx:                150 * side,
+		dy:                0,
+		speedupMultiplier: 1.001,
+	}
+}
